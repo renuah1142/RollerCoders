967feb3d5d8fa906495cbe9e2e8c330893afce8a
Grubkid <122883674+Grubkid@users.noreply.github.com>
2026-04-09 00:42:42 -0400

    Change password hashing method in tests
    Update password hashing method from 'sha256' to 'pbkdf2:sha256'.

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -33,7 +33,7 @@ class UserUnitTests(unittest.TestCase):
     
     def test_hashed_password(self):
         password = "mypass"
-        hashed = generate_password_hash(password, method='sha256')
+        generate_password_hash(password, method='pbkdf2:sha256')
         user = User("bob", password)
         assert user.password != password
 
